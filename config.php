e3313379a6a30731d19bade50c8276aaab0b3f64
Lok Prakash Pandey <lokprakashpandey@yahoo.com>
2018-04-14 18:08:38 -0400

    Add files via upload

--- config.php
+++ config.php
@@ -5,8 +5,8 @@ session_start();
 require_once "Facebook/autoload.php";
 
 $FB = new \Facebook\Facebook([
-	'app_id' => '172393383465172',
-	'app_secret' => '303d0e281209901ad0192e5809fc5b26',
+	'app_id' => '',
+	'app_secret' => '',
 	'default_graph_version' => 'v2.10',
 	]);
 
